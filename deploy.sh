ce22dddc036231dd0ecdd2e0a41cd47e6102d0ec
Liam Brannigan <l.brannigan@analyticsengines.com>
2022-11-23 14:23:04 +0000

    added key concetps and previous changes

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 #   Copyright (C) 2022 Liam Brannigan
 #
 
-DOCKER_BUILDKIT=1 docker build -t udemy-pandas-to-polars .
+DOCKER_BUILDKIT=1 docker build --no-cache -t udemy-pandas-to-polars .
 
 docker run -it --rm -p 8888:8888  -v $(pwd):/usr/src/app udemy-pandas-to-polars:latest /bin/bash
